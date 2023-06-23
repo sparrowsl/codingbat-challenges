c9bd876572be5fe77dab1209d1ccdb39da30153f
sparrowsl <benthorpe.dev@gmail.com>
2023-06-23 12:23:59 +0000

    add some warmup_1 challenges with unit tests

---
+++ warmup1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Warmup 1")
+}
+
+// The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation.
+// We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+func SleepIn(weekday, vacation bool) bool {
+	return (!weekday || vacation)
+}
+
+// We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling.
+// We are in trouble if they are both smiling or if neither of them is smiling.
+// Return True if we are in trouble.
+func MonkeyTrouble(a_smile, b_smile bool) bool {
+	return (a_smile == b_smile)
+}
+
+// Given two int values, return their sum.
+// Unless the two values are the same, then return double their sum.
+func SumDouble(a, b int) int {
+	if a == b {
+		return (a + b) * 2
+	}
+	return a + b
+}
+
+// Given an int n, return the absolute difference between n and 21,
+// except return double the absolute difference if n is over 21.
+func Diff21(n int) int {
+	if n <= 21 {
+		return 21 - n
+	} else {
+		return (n - 21) * 2
+	}
+}
+
+// Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+func Makes10(a, b int) bool {
+	return a == 10 || b == 10 || (a+b) == 10
+}
+
+// Given an int n, return True if it is within 10 of 100 or 200.
+// Note: abs(num) computes the absolute value of a number.
+func NearHundred(n int) bool {
+	return (math.Abs(100-float64(n)) <= 10) || (math.Abs(200-float64(n)) <= 10)
+}
+
+// Given 2 int values, return True if one is negative and one is positive.
+// Except if the parameter "negative" is True, then return True only if both are negative.
+func PosNeg(a, b int, negative bool) bool {
+	if negative {
+		return (a < 0 && b < 0)
+	} else {
+		return (a < 0 && b >= 0) || (a >= 0 && b < 0)
+	}
+}
---
+++ warmup1/main_test.go
@@ -0,0 +1,140 @@
+package main
+
+import "testing"
+
+func TestSleepIn(t *testing.T) {
+	sleepCases := []struct {
+		a, b, c bool
+	}{
+		{false, false, true},
+		{true, false, false},
+		{false, true, true},
+		{true, true, true},
+	}
+
+	for _, v := range sleepCases {
+		ans := SleepIn(v.a, v.b)
+
+		if ans != v.c {
+			t.Error("For", v, "Expected", v.c, "got", ans)
+		}
+	}
+}
+
+func TestMonkeyTrouble(t *testing.T) {
+	monkeyCases := []struct {
+		a, b, c bool
+	}{
+		{true, true, true},
+		{false, false, true},
+		{true, false, false},
+		{false, true, false},
+	}
+
+	for _, v := range monkeyCases {
+		ans := MonkeyTrouble(v.a, v.b)
+
+		if ans != v.c {
+			t.Error("For", v, "Expected", v.c, "got", ans)
+		}
+	}
+}
+
+func TestSumDouble(t *testing.T) {
+	doubleCases := []struct {
+		a, b, ans int
+	}{
+		{1, 2, 3},
+		{3, 2, 5},
+		{2, 2, 8},
+		{-1, 0, -1},
+		{3, 3, 12},
+		{0, 0, 0},
+	}
+
+	for _, v := range doubleCases {
+		sum := SumDouble(v.a, v.b)
+
+		if sum != v.ans {
+			t.Error("For", v, "Expected", v.ans, "got", sum)
+		}
+	}
+}
+
+func TestDiff21(t *testing.T) {
+	diffCases := []struct {
+		n, ans int
+	}{
+		{19, 2},
+		{10, 11},
+		{21, 0},
+		{22, 2},
+		{25, 8},
+		{0, 21},
+	}
+
+	for _, diff := range diffCases {
+		ans := Diff21(diff.n)
+
+		if ans != diff.ans {
+			t.Error("For", diff.n, "Expected", diff.ans, "got", ans)
+		}
+	}
+}
+
+func TestMakes10(t *testing.T) {
+	numbers := []struct {
+		a, b int
+		ans  bool
+	}{
+		{9, 10, true},
+		{9, 9, false},
+		{1, 9, true},
+	}
+
+	for _, v := range numbers {
+		ans := Makes10(v.a, v.b)
+
+		if ans != v.ans {
+			t.Error("For", v, "Expected", v.ans, "got", ans)
+		}
+	}
+}
+
+func TestNearHundred(t *testing.T) {
+	nums := []struct {
+		n   int
+		ans bool
+	}{
+		{93, true},
+		{90, true},
+		{89, false},
+	}
+
+	for _, num := range nums {
+		v := NearHundred(num.n)
+
+		if v != num.ans {
+			t.Error("For", num, "Expected", num.ans, "got", v)
+		}
+	}
+}
+
+func TestPosNeg(t *testing.T) {
+	posNegs := []struct {
+		a, b          int
+		negative, ans bool
+	}{
+		{1, -1, false, true},
+		{-1, 1, false, true},
+		{-4, -5, true, true},
+	}
+
+	for _, n := range posNegs {
+		v := PosNeg(n.a, n.b, n.negative)
+
+		if v != n.ans {
+			t.Error("For", n, "Expected", n.ans, "got", v)
+		}
+	}
+}
