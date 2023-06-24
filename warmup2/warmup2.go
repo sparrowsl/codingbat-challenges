0aa0dabcfa2863d6bd8cc8534924f898ac7448c8
sparrowsl <benthorpe.dev@gmail.com>
2023-06-24 23:50:40 +0000

    commit second function

--- warmup2/warmup2.go
+++ warmup2/warmup2.go
@@ -13,3 +13,11 @@ func main() {
 func StringTimes(word string, n uint) string {
 	return strings.Repeat(word, int(n))
 }
+
+// Given a string and a non-negative int n,
+// we'll say that the front of the string is the first 3 chars,
+// or whatever is there if the string is less than length 3.
+// Return n copies of the front;
+func FrontTimes(word string, n uint) string {
+	return strings.Repeat(word[:3], int(n))
+}
--- warmup2/warmup2_test.go
+++ warmup2/warmup2_test.go
@@ -21,3 +21,23 @@ func TestStringTimes(t *testing.T) {
 		}
 	}
 }
+
+func TestFrontTimes(t *testing.T){
+	testCases := []struct {
+		word     string
+		n        uint
+		expected string
+	}{
+		{"Chocolate", 2, "ChoCho"},
+		{"Chocolate", 3, "ChoChoCho"},
+		{"Abc", 3, "AbcAbcAbc"},
+	}
+
+	for _,test := range testCases{
+		result := FrontTimes(test.word,test.n)
+
+				if result != test.expected {
+			t.Errorf("FrontTimes(%q, %v) FAILED. Expected %v, got %v", test.word, test.n, test.expected, result)
+		}
+	}
+}
