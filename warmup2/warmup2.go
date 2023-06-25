b70b0c5be9f54ce4cd3159105baaf779a95c9a2a
sparrowsl <benthorpe.dev@gmail.com>
2023-06-25 02:54:33 +0000

    add another function test

--- warmup2/warmup2.go
+++ warmup2/warmup2.go
@@ -9,7 +9,8 @@ func main() {
 	fmt.Println("CodingBat Warmup 2 in Go")
 }
 
-// Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+// Given a string and a non-negative int n,
+// return a larger string that is n copies of the original string.
 func StringTimes(word string, n uint) string {
 	return strings.Repeat(word, int(n))
 }
@@ -20,4 +21,36 @@ func StringTimes(word string, n uint) string {
 // Return n copies of the front;
 func FrontTimes(word string, n uint) string {
 	return strings.Repeat(word[:3], int(n))
+}
+
+// Given a string, return a new string made of every other char starting with the first,
+// so "Hello" yields "Hlo".
+func StringBits(word string) string {
+	var final string
+
+	for i, char := range word {
+		if i%2 == 0 {
+			final += string(char)
+		}
+	}
+	return final
+}
+
+// Given an array of ints, return the number of 9's in the array.
+func arrayCount(array []int) int {
+	var nines int
+
+	for _, n := range array {
+		if n == 9 {
+			nines += 1
+		}
+	}
+
+	return nines
+}
+
+// Given an array of ints, return True if one of the first 4 elements in the array is a 9.
+// The array length may be less than 4.
+func arrayFront(array []int) bool {
+	return false
 }
--- warmup2/warmup2_test.go
+++ warmup2/warmup2_test.go
@@ -22,7 +22,7 @@ func TestStringTimes(t *testing.T) {
 	}
 }
 
-func TestFrontTimes(t *testing.T){
+func TestFrontTimes(t *testing.T) {
 	testCases := []struct {
 		word     string
 		n        uint
@@ -33,11 +33,68 @@ func TestFrontTimes(t *testing.T){
 		{"Abc", 3, "AbcAbcAbc"},
 	}
 
-	for _,test := range testCases{
-		result := FrontTimes(test.word,test.n)
+	for _, test := range testCases {
+		result := FrontTimes(test.word, test.n)
 
-				if result != test.expected {
+		if result != test.expected {
 			t.Errorf("FrontTimes(%q, %v) FAILED. Expected %v, got %v", test.word, test.n, test.expected, result)
+		}
+	}
+}
+
+func TestStringBits(t *testing.T) {
+	testCases := []struct {
+		word, expected string
+	}{
+		{"Hello", "Hlo"},
+		{"Hi", "H"},
+		{"Heeololeo", "Hello"},
+	}
+
+	for _, test := range testCases {
+		result := StringBits(test.word)
+
+		if result != test.expected {
+			t.Errorf("FrontTimes(%q) FAILED. Expected %q, got %q", test.word, test.expected, result)
+		}
+	}
+}
+
+func Test_arrayCount(t *testing.T) {
+	testCases := []struct {
+		array    []int
+		expected int
+	}{
+		{[]int{1, 2, 9}, 1},
+		{[]int{1, 9, 9}, 2},
+		{[]int{1, 9, 9, 3, 9}, 3},
+		{[]int{}, 0},
+	}
+
+	for _, test := range testCases {
+		result := arrayCount(test.array)
+
+		if result != test.expected {
+			t.Errorf("arrayCount(%v) FAILED. Expected %v, got %v", test.array, test.expected, result)
+		}
+	}
+}
+
+func Test_arrayFront(t *testing.T) {
+	testCases := []struct {
+		array    []int
+		expected bool
+	}{
+		{[]int{1, 2, 9, 3, 4}, true},
+		{[]int{1, 2, 3, 4, 9}, false},
+		{[]int{1, 2, 3, 4, 5}, false},
+	}
+
+	for _, test := range testCases {
+		result := arrayFront(test.array)
+
+		if result != test.expected {
+			t.Errorf("arrayFront(%v) FAILED. Expected %v, got %v", test.array, test.expected, result)
 		}
 	}
 }
